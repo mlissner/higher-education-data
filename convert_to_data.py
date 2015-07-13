5e03368f9305b9fe2870477ac1a3c9abe9553aba
Michael Lissner <mlissner@michaeljaylissner.com>
2015-07-13 11:57:41 -0700

    Tweaks it so that we only get data from schools that offer high enough degrees. This elimates the many, many beauty and other vocational schools, halving the data size.

--- convert_to_data.py
+++ convert_to_data.py
@@ -7,33 +7,36 @@ with open('./data/DOE/data-2009-2010.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     pk = 1
     for row in reader:
-        school = {
-            'pk': pk,
-            'model': 'judges.School',
-            'fields': {
-                'name':    row['INSTNM'].decode('latin-1').strip(),
-                'unit_id': int(row['UNITID'].decode('latin-1').strip()),
-                'ein':     int(row['EIN'].decode('latin-1').strip()),
-                'ope_id':  int(row['OPEID'].decode('latin-1').strip()),
-            }
-        }
-
-        schools.append(school)
-        root_pk = pk
-        pk += 1
-        clean_aliases = [alias.decode('latin-1').strip() for alias in
-                         row['IALIAS'].split('|') if alias.strip()]
-        for alias in clean_aliases:
+        high_enough_level_offered = (int(row['HLOFFER']) >= 4)
+        more_than_post_secondary_offered = (int(row['DEGGRANT']) in [1, -3])
+        if all([high_enough_level_offered, more_than_post_secondary_offered]):
             school = {
                 'pk': pk,
                 'model': 'judges.School',
                 'fields': {
-                    'name': alias,
-                    'is_alias_of': root_pk,
+                    'name':    row['INSTNM'].decode('latin-1').strip(),
+                    'unit_id': int(row['UNITID'].decode('latin-1').strip()),
+                    'ein':     int(row['EIN'].decode('latin-1').strip()),
+                    'ope_id':  int(row['OPEID'].decode('latin-1').strip()),
                 }
             }
+
             schools.append(school)
+            root_pk = pk
             pk += 1
+            clean_aliases = [alias.decode('latin-1').strip() for alias in
+                             row['IALIAS'].split('|') if alias.strip()]
+            for alias in clean_aliases:
+                school = {
+                    'pk': pk,
+                    'model': 'judges.School',
+                    'fields': {
+                        'name': alias,
+                        'is_alias_of': root_pk,
+                    }
+                }
+                schools.append(school)
+                pk += 1
 
 with open('./school_data.json', 'wb') as out:
     json.dump(schools, out, indent=2)
